b01c9bd1cc7182d8bf66d7f28b7d00a95d5673a0
Sterling Coleman <sterling.coleman@algoan.com>
2020-04-23 10:58:24 +0200

    improve unit tests

--- src/app/components/users/list/list.component.spec.ts
+++ src/app/components/users/list/list.component.spec.ts
@@ -1,41 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import * as firebase from 'firebase';
-import { BehaviorSubject } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { MatListModule } from '@angular/material/list';
+import { of } from 'rxjs';
 
 import { UserListComponent } from './list.component';
+import { UserService } from '../shared/users.service';
+import { User } from '../shared/user.model';
 
 describe('ListComponent', () => {
   let fixture: ComponentFixture<UserListComponent>;
-  const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    birthTimeStamp: firebase.firestore.Timestamp.fromDate(new Date()),
+  const user: User = {
+    firstName: 'Service',
+    lastName: 'Test',
+    birthDate: new Date(),
     email: 'johndoemtc@dispostable.com',
     phone: '+15555555555',
   };
 
-  // Create a stub which replaces the call to the real Firebase
-  const FirestoreStub = {
-    collection: (name: string) => ({
-      valueChanges: () => new BehaviorSubject([user]),
-    }),
-  };
+  // Mock the service, return the above user instead
+  const userService = jasmine.createSpyObj('UserService', ['getUsers']);
+  const spy = userService.getUsers.and.returnValue(of([user]));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UserListComponent],
-      providers: [{ provide: AngularFirestore, useValue: FirestoreStub }],
+      imports: [MatListModule],
+      providers: [{ provide: UserService, useValue: userService }],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
-    fixture.detectChanges();
-  });
+  }));
 
   // Expect one user in the list, per the stub
   it('should display list', () => {
+    fixture.detectChanges();
+    expect(spy.calls.any()).toBe(true, 'getUsers called');
     expect(
       fixture.nativeElement.querySelector('mat-list').children.length
     ).toEqual(1);
@@ -44,8 +42,6 @@ describe('ListComponent', () => {
     expect(child.textContent).toContain(user.lastName);
     expect(child.textContent).toContain(user.email);
     expect(child.textContent).toContain(user.phone);
-    expect(child.textContent).toContain(
-      user.birthTimeStamp.toDate().toLocaleDateString()
-    );
+    expect(child.textContent).toContain(user.birthDate.toLocaleDateString());
   });
 });
--- src/app/components/users/shared/users.service.spec.ts
+++ src/app/components/users/shared/users.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
+import { BehaviorSubject } from 'rxjs';
 
 import { UserService } from './users.service';
 
-describe('UsersService', () => {
+describe('UserService', () => {
   let service: UserService;
+  const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthTimeStamp: firebase.firestore.Timestamp.fromDate(new Date()),
+    email: 'johndoemtc2@dispostable.com',
+    phone: '+15555555555',
+  };
 
   beforeEach(() => {
     // Stub the firestore to mock any response
     const FirestoreStub = {
       collection: (name: string) => ({
-        doc: (id: string) => ({}),
+        valueChanges: () => new BehaviorSubject([user]),
       }),
     };
     TestBed.configureTestingModule({
@@ -19,7 +28,10 @@ describe('UsersService', () => {
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get users', () => {
+    service.getUsers().subscribe((users) => {
+      expect(users.length).toEqual(1);
+      expect(users[0].birthDate).toEqual(user.birthTimeStamp.toDate());
+    });
   });
 });
